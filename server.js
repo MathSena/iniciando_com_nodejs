6ab5d1b4a78ac4f600462ec10503e08378c59496
MathSena <MathSena07@hotmail.com>
2022-02-03 21:52:26 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const http = require('http')
+
+http
+  .createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'application/json' })
+
+    if (request.url === '/produto') {
+      response.end(
+        JSON.stringify({
+          message: 'Rota de produto'
+        })
+      )
+    }
+
+    if (request.url === '/usuarios') {
+      response.end(
+        JSON.stringify({
+          message: 'Rota de usuário'
+        })
+      )
+    }
+    response.end(
+      JSON.stringify({
+        message: 'Qualquer outra rota'
+      })
+    )
+  })
+  .listen(4001, () => console.log('Servidor rodando na porta 4001'))
